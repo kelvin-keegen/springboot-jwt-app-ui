137339d5d21f81b653bcdac85ff5eb62413e2d8f
Kelvin Keegan <kelvinkeegen17@gmail.com>
2022-03-14 12:42:28 +0300

    Added Ui components on Password change and reset views

--- src/main/java/com/example/application/views/passwordchange/PasswordChangeView.java
+++ src/main/java/com/example/application/views/passwordchange/PasswordChangeView.java
@@ -1,9 +1,10 @@
 package com.example.application.views.passwordchange;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -13,6 +14,26 @@ public class PasswordChangeView extends VerticalLayout {
 
     public PasswordChangeView() {
 
+        TextField textFieldEmail = new TextField("Email *");
+        TextField textFieldOldPWD = new TextField("Old Password *");
+        TextField textFieldNewPWD = new TextField("New Password *");
+        TextField textFieldVeryPWD = new TextField("Confirm Password *");
+
+        Button buttonChange = new Button("Please change my Password");
+        buttonChange.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        add(
+                new H1("Password Change"),
+                new H5("Please make sure to remember your new password upon change."),
+                textFieldEmail,
+                textFieldOldPWD,
+                textFieldNewPWD,
+                textFieldVeryPWD,
+                buttonChange,
+                new H5("A confirmation will be displayed upon successful password change")
+        );
+
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
     }
 
 }
--- src/main/java/com/example/application/views/passwordreset/PasswordResetView.java
+++ src/main/java/com/example/application/views/passwordreset/PasswordResetView.java
@@ -1,17 +1,36 @@
 package com.example.application.views.passwordreset;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import lombok.extern.slf4j.Slf4j;
 
 @PageTitle("Password Reset")
 @Route(value = "password-reset")
+@Slf4j
 public class PasswordResetView extends VerticalLayout {
 
     public PasswordResetView() {
+
+        TextField textFieldEmail = new TextField("Email *");
+
+        Button buttonReset = new Button("Reset");
+        buttonReset.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        add(
+                new H1("Password Reset"),
+                new H5("Enter your email address for account verification."),
+                textFieldEmail,
+                buttonReset,
+                new H5("Please check your email inbox for the randomly generated password.")
+
+                );
+
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
 
     }
 
